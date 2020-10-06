e0eab94c736e99b01eebf8573c2b1c76f1de17c8
Charles-Antoine <70984285+Charles-Antoine23@users.noreply.github.com>
2020-10-06 18:51:26 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
+def ellipsoide( x = 1, y = 1, z = 1, masse_volumique = 1):
 
+    volume = 4/3 * x * y * z
+    masse = masse_volumique * volume
+
+    return (volume, masse)
 
 
 # TODO: Définissez vos fonction ici
@@ -10,5 +15,7 @@
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
+
+    print(ellipsoide())
 
     pass
